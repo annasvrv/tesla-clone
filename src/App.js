ca9146f0dea6efa2f957963c545dc3aae0eeab6c
Anna <anna.svrva@gmail.com>
2024-02-29 17:28:04 +0200

    added sections in Tesla

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <Header /> */}
+      <Home />
+    </div>
+  );
 }
 
 export default App;

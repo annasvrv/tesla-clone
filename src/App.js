2e05ac1da39b05f559a60614baca2d07c777f9ab
Anna <anna.svrva@gmail.com>
2024-03-27 19:56:17 +0200

    added redux carSlice  state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Provider store={store}>
+        <Header />
+        <Home />
+      </Provider>
     </div>
   );
 }
